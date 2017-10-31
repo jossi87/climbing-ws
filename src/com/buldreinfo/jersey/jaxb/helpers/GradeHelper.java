fe8ed7cf0bef59f2ef1fa4a67556d7d8fe85cfd2
Jostein Øygarden <jostein.oygarden@verico.com>
2017-10-31 12:01:50 +0100

    grades

--- src/com/buldreinfo/jersey/jaxb/helpers/GradeHelper.java
+++ src/com/buldreinfo/jersey/jaxb/helpers/GradeHelper.java
@@ -11,19 +11,59 @@ public class GradeHelper {
 	public static ImmutableBiMap<Integer, String> getGrades(int regionId) {
 		Map<Integer, String> map = new LinkedHashMap<>();
 		if (regionId == 4) {
-			map.put(19, "10");
-			map.put(18, "9+");
-			map.put(17, "9");
-			map.put(16, "8+");
-			map.put(15, "8");
-			map.put(14, "7+");
-			map.put(13, "7");
-			map.put(12, "6+");
-			map.put(11, "6");
-			map.put(10, "5+");
-			map.put(9, "5");
-			map.put(8, "4+");
-			map.put(7, "4");
+			map.put(53, "9+");
+			map.put(52, "9 / 9+");
+			map.put(51, "9");
+			map.put(50, "9- / 9");
+			map.put(49, "9-");
+			map.put(48, "8+ / 9-");
+			map.put(47, "8+");
+			map.put(46, "8 / 8+");
+			map.put(45, "8");
+			map.put(44, "8- / 8");
+			map.put(43, "8-");
+			map.put(42, "7+ / 8-");
+			map.put(41, "7+");
+			map.put(40, "7 / 7+");
+			map.put(39, "7");
+			map.put(38, "7- / 7");
+			map.put(37, "7-");
+			map.put(36, "6+ / 7-");
+			map.put(35, "6+");
+			map.put(34, "6 / 6+");
+			map.put(33, "6");
+			map.put(32, "6- / 6");
+			map.put(31, "6-");
+			map.put(30, "5+ / 6-");
+			map.put(29, "5+");
+			//			map.put(28, "5 / 5+");
+			map.put(27, "5");
+			//			map.put(26, "5- / 5");
+			map.put(25, "5-");
+			//			map.put(24, "4+ / 5-");
+			map.put(23, "4+");
+			//			map.put(22, "4 / 4+");
+			map.put(21, "4");
+			//			map.put(20, "4- / 4");
+			map.put(19, "4-");
+			//			map.put(18, "3+ / 4-");
+			map.put(17, "3+");
+			//			map.put(16, "3 / 3+");
+			map.put(15, "3");
+			//			map.put(14, "3- / 3");
+			map.put(13, "3-");
+			//			map.put(12, "2+ / 3-");
+			//			map.put(11, "2+");
+			//			map.put(10, "2 / 2+");
+			//			map.put(9, "2");
+			//			map.put(8, "2- / 2");
+			//			map.put(7, "2-");
+			//			map.put(6, "1+ / 2-");
+			//			map.put(5, "1+");
+			//			map.put(4, "1 / 1+");
+			//			map.put(3, "1");
+			//			map.put(2, "1- / 1");
+			//			map.put(1, "1-");
 			map.put(0, "n/a");
 		}
 		else {
@@ -53,11 +93,11 @@ public class GradeHelper {
 		}
 		return ImmutableBiMap.copyOf(map);
 	}
-	
+
 	public static String intToString(int regionId, int grade) throws SQLException {
 		return Preconditions.checkNotNull(getGrades(regionId).get(grade), "Invalid grade=" + grade);
 	}
-	
+
 	public static int stringToInt(int regionId, String grade) throws SQLException {
 		Preconditions.checkNotNull(grade, "grade is null");
 		return getGrades(regionId).inverse().get(grade);
