45ed1232bfec01526a80aedfe8e09769cd8c49f7
Jostein Øygarden <jostein.oygarden@verico.com>
2018-02-20 19:13:43 +0100

    svg

--- src/com/buldreinfo/jersey/jaxb/batch/SvgParser.java
+++ src/com/buldreinfo/jersey/jaxb/batch/SvgParser.java
@@ -7,12 +7,34 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import jersey.repackaged.com.google.common.base.Preconditions;
+
 public class SvgParser {
+	public static void main(String[] args) throws Exception {
+		Path svg = Paths.get("C:/Users/jostein/Dropbox/klatrefører/Bersagel/Storveggen.svg");
+		new SvgParser(svg);
+	}
+	
 	public SvgParser(Path svg) throws Exception {
 		List<com.buldreinfo.jersey.jaxb.batch.svg.Path> paths = new ArrayList<>();
 		List<String> lines = Files.readAllLines(svg);
+		double[][] matrix = null;
 		for (int i = 0; i < lines.size(); i++) {
 			String l = lines.get(i);
+			if (l.contains("image style") && l.contains("matrix(")) {
+				Preconditions.checkArgument(matrix == null);
+				matrix = new double[2][3];
+				String m = l.substring(l.indexOf("matrix(") + 7);
+				m = m.replace(")\">", "");
+				String[] parts = m.split(" ");
+				Preconditions.checkArgument(parts.length == 6, "Invalid matrix: " + m);
+				matrix[0][0] = Double.parseDouble(parts[0]); // a
+				matrix[1][0] = Double.parseDouble(parts[1]); // b
+				matrix[0][1] = Double.parseDouble(parts[2]); // c
+				matrix[1][1] = Double.parseDouble(parts[3]); // d
+				matrix[0][2] = Double.parseDouble(parts[4]); // e
+				matrix[1][2] = Double.parseDouble(parts[5]); // f
+			}
 			int ix = l.indexOf("<path ");
 			if (ix > -1) {
 				String r = l.substring(l.indexOf(" d=\"")+4);
@@ -21,25 +43,22 @@ public class SvgParser {
 					r += lines.get(i+(diff++)).trim();
 				}
 				r = r.replace("\"/>", "");
-				
-				double[][] matrix = new double[2][3]; // TODO FIX
-				matrix[0][0] = 0.1532; // a
-				matrix[1][0] = 0; // b
-				matrix[0][1] = 0; // c
-				matrix[1][1] = 0.1532; // d
-				matrix[0][2] = 58; // e
-				matrix[1][2] = -17.6689; // f
 				paths.add(new com.buldreinfo.jersey.jaxb.batch.svg.Path(r, matrix));
+			}
+			ix = l.indexOf("<polyline ");
+			if (ix > -1) {
+				String r = l.substring(l.indexOf(" points=\"")+9);
+				int diff = 1;
+				while (!r.endsWith("/>")) {
+					r += lines.get(i+(diff++)).trim();
+				}
+				r = r.replace("\"/>", "");
+				paths.add(new com.buldreinfo.jersey.jaxb.batch.svg.Path("M" + r, matrix));
 			}
 		}
 		Collections.sort(paths, (p1, p2) -> Double.compare(p1.getMinX(), p2.getMinX()));
 		for (com.buldreinfo.jersey.jaxb.batch.svg.Path p : paths) {
 			System.out.println(p);
 		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		Path svg = Paths.get("C:/Users/jostein/Dropbox/klatrefører/Bersagel/Storveggen.svg");
-		new SvgParser(svg);
 	}
 }
--- src/com/buldreinfo/jersey/jaxb/batch/svg/Path.java
+++ src/com/buldreinfo/jersey/jaxb/batch/svg/Path.java
@@ -28,12 +28,27 @@ public class Path {
 		addElement(temp);
 	}
 
+	public double getMinX() {
+		return elements.stream().min((a, b) -> Double.compare(a.getX(), b.getX())).get().getX();
+	}
+	
+	@Override
+	public String toString() {
+		for (Element e : elements) {
+			e.applyMatrix(matrix);
+		}
+		return Joiner.on(" ").join(elements).replaceAll(",", ".");
+	}
+
 	private void addElement(String temp) {
 		switch (temp.charAt(0)) {
 		case 'M':
 			String[] parts = temp.substring(1).trim().split(" ");
-			Preconditions.checkArgument(parts.length == 2, "M: Invalid parts=" + temp.substring(1) + ", path=" + path);
-			elements.add(new M(Double.parseDouble(parts[0]), Double.parseDouble(parts[1])));
+			for (int i = 0; i < parts.length; i+=2) {
+				double x = Double.parseDouble(parts[i]);
+				double y = Double.parseDouble(parts[i+1]);
+				elements.add(new M(x, y));
+			}
 			break;
 		case 'l':
 			parts = temp.substring(1).trim().split(" ");
@@ -50,29 +65,35 @@ public class Path {
 			for (int i = 0; i < parts.length; i+=6) {
 				double startX = elements.get(elements.size()-1).getX();
 				double startY = elements.get(elements.size()-1).getY();
-				double x = startX + Double.parseDouble(parts[i+4]);
-				double y = startY + Double.parseDouble(parts[i+5]);
 				double x1 = startX + Double.parseDouble(parts[i]);
 				double y1 = startY + Double.parseDouble(parts[i+1]);
 				double x2 = startX + Double.parseDouble(parts[i+2]);
 				double y2 = startY + Double.parseDouble(parts[i+3]);
+				double x = startX + Double.parseDouble(parts[i+4]);
+				double y = startY + Double.parseDouble(parts[i+5]);
+				elements.add(new C(x1, y1, x2, y2, x, y));
+			}
+			break;
+		case 's':
+			parts = temp.substring(1).trim().split(" ");
+			for (int i = 0; i < parts.length; i+=4) {
+				Element prevElement = elements.get(elements.size()-1);
+				double startX = prevElement.getX();
+				double startY = prevElement.getY();
+				double x2 = startX + Double.parseDouble(parts[i]);
+				double y2 = startY + Double.parseDouble(parts[i+1]);
+				double x = startX + Double.parseDouble(parts[i+2]);
+				double y = startY + Double.parseDouble(parts[i+3]);
+				double x1 = 0;
+				double y1 = 0;
+					x1 = x;
+					y1 = y;
+				
 				elements.add(new C(x1, y1, x2, y2, x, y));
 			}
 			break;
 		default:
 			throw new RuntimeException("Invalid temp=" + temp);
 		}
-	}
-	
-	@Override
-	public String toString() {
-		for (Element e : elements) {
-			e.applyMatrix(matrix);
-		}
-		return Joiner.on(" ").join(elements).replaceAll(",", ".");
-	}
-
-	public double getMinX() {
-		return elements.stream().min((a, b) -> Double.compare(a.getX(), b.getX())).get().getX();
 	}
 }
