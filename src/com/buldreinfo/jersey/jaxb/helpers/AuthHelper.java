98ddab74eedd892cbdb5263045eeff9cfce12389
Jostein Øygarden <jostein.oygarden@verico.com>
2018-08-01 16:28:34 +0200

    log

--- src/com/buldreinfo/jersey/jaxb/helpers/AuthHelper.java
+++ src/com/buldreinfo/jersey/jaxb/helpers/AuthHelper.java
@@ -2,12 +2,18 @@ package com.buldreinfo.jersey.jaxb.helpers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.auth0.client.auth.AuthAPI;
 import com.auth0.json.auth.UserInfo;
 import com.auth0.net.Request;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.common.net.HttpHeaders;
 
 public class AuthHelper {
+	private static Logger logger = LogManager.getLogger();
 	private final AuthAPI auth;
 
 	public AuthHelper() {
@@ -17,10 +23,16 @@ public class AuthHelper {
 	public int getUserId(HttpServletRequest request) {
 		try {
 			String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
-			Request<UserInfo> req = auth.userInfo(authorization.substring(7));
-			UserInfo info = req.execute();
-			String sub = (String) info.getValues().get("sub");
-			return Integer.parseInt(sub.replace("auth0|",""));
+			Preconditions.checkArgument(!Strings.isNullOrEmpty(authorization));
+			try {
+				Request<UserInfo> req = auth.userInfo(authorization.substring(7));
+				UserInfo info = req.execute();
+				String sub = (String) info.getValues().get("sub");
+				return Integer.parseInt(sub.replace("auth0|",""));
+			} catch (Exception ex) {
+				logger.fatal(ex.getMessage(), ex);
+				throw ex;
+			}
 		} catch (Exception e) {
 			return -1;
 		}
