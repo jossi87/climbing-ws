7349a4cdb96258e375dc4e6b6cabf92189946bc9
Jostein Øygarden <jostein.oygarden@verico.com>
2018-08-01 16:39:19 +0200

    x

--- src/com/buldreinfo/jersey/jaxb/helpers/AuthHelper.java
+++ src/com/buldreinfo/jersey/jaxb/helpers/AuthHelper.java
@@ -1,5 +1,7 @@
 package com.buldreinfo.jersey.jaxb.helpers;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
@@ -10,29 +12,37 @@ import com.auth0.json.auth.UserInfo;
 import com.auth0.net.Request;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.net.HttpHeaders;
 
 public class AuthHelper {
 	private static Logger logger = LogManager.getLogger();
-	private final AuthAPI auth;
+	private static AuthAPI auth = new AuthAPI("buldreinfo.auth0.com", "zexpFfou6HkgNWH5QVi3zyT1rrw6MXAn", "Yi7viH5URp9kJO0LhvSRQS-8Y6F2BR6_UIdx96KhbhtsbOe9HtFtOBcl6v55iT7o");
+	private static LoadingCache<String, Integer> cache = CacheBuilder.newBuilder()
+			.maximumSize(1000)
+			.expireAfterWrite(4, TimeUnit.HOURS)
+			.build(new CacheLoader<String, Integer>() {
+				@Override
+				public Integer load(String authorization) throws Exception {
+					try {
+						Request<UserInfo> req = auth.userInfo(authorization.substring(7));
+						UserInfo info = req.execute();
+						String sub = (String) info.getValues().get("sub");
+						return Integer.parseInt(sub.replace("auth0|",""));
+					} catch (Exception ex) {
+						logger.warn(ex.getMessage(), ex);
+						throw ex;
+					}
+				}
+			});
 
-	public AuthHelper() {
-		this.auth = new AuthAPI("buldreinfo.auth0.com", "zexpFfou6HkgNWH5QVi3zyT1rrw6MXAn", "Yi7viH5URp9kJO0LhvSRQS-8Y6F2BR6_UIdx96KhbhtsbOe9HtFtOBcl6v55iT7o");
-	}
-	
 	public int getUserId(HttpServletRequest request) {
 		try {
 			String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
 			Preconditions.checkArgument(!Strings.isNullOrEmpty(authorization));
-			try {
-				Request<UserInfo> req = auth.userInfo(authorization.substring(7));
-				UserInfo info = req.execute();
-				String sub = (String) info.getValues().get("sub");
-				return Integer.parseInt(sub.replace("auth0|",""));
-			} catch (Exception ex) {
-				logger.fatal(ex.getMessage(), ex);
-				throw ex;
-			}
+			return cache.get(authorization);
 		} catch (Exception e) {
 			return -1;
 		}
