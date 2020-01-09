bdaa579caa166a140eb99cc5c741bd56106eeda8
Jostein Øygarden <jostein.oygarden@gmail.com>
2020-01-09 14:54:58 +0100

    x

--- src/com/buldreinfo/jersey/jaxb/db/BuldreinfoRepository.java
+++ src/com/buldreinfo/jersey/jaxb/db/BuldreinfoRepository.java
@@ -1805,7 +1805,7 @@ public class BuldreinfoRepository {
 			ps = null;
 			Preconditions.checkArgument(idMedia > 0);
 			if (idProblem > 0) {
-				ps = c.getConnection().prepareStatement("INSERT INTO media_problem (media_id, problem_id, pitch) VALUES (?, ?, ?)");
+				ps = c.getConnection().prepareStatement("INSERT INTO media_problem (media_id, problem_id, pitch) VALUES (?, ?, ?, ?)");
 				ps.setInt(1, idMedia);
 				ps.setInt(2, idProblem);
 				if (pitch > 0) {
