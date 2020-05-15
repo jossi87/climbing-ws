53d78791a5e72e1cdf4e21c8b8f73b9baea5ca31
jostein <jostein.oygarden@verico.com>
2020-05-15 13:20:02 +0200

    x

--- src/com/buldreinfo/jersey/jaxb/db/ConnectionPool.java
+++ src/com/buldreinfo/jersey/jaxb/db/ConnectionPool.java
@@ -9,14 +9,13 @@ import org.apache.commons.dbcp2.BasicDataSource;
  */
 public class ConnectionPool {
 	private static final String HOST = "172.104.157.185";
-	private static final String DATABASE = "localhost";
+	private static final String DATABASE = "buldreinfo";
 	private static final String USER = "buldreinfo";
 	private static final String PASSWORD = "5459OoiwqQwerJgfg12_224WrejvJqGhhJ";
 	private final BasicDataSource bds;
 	
 	public ConnectionPool() {
 		this.bds = new BasicDataSource();
-		this.bds.setDriverClassName("com.mysql.jdbc.Driver");
 		this.bds.setUrl(String.format("jdbc:mysql://%s/%s?user=%s&password=%s", HOST, DATABASE, USER, PASSWORD));
 	}
 	
