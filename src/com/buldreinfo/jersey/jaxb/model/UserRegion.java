10dc54b676afe53f70f1f065aac7c8c8331ad4a5
Jostein Øygarden <jostein.oygarden@verico.com>
2020-10-21 08:35:07 +0200

    x

--- src/com/buldreinfo/jersey/jaxb/model/UserRegion.java
+++ src/com/buldreinfo/jersey/jaxb/model/UserRegion.java
@@ -3,12 +3,14 @@ package com.buldreinfo.jersey.jaxb.model;
 public class UserRegion {
 	private final int id;
 	private final String name;
+	private final String role;
 	private final boolean enabled;
 	private final boolean readOnly;
 	
-	public UserRegion(int id, String name, boolean enabled, boolean readOnly) {
+	public UserRegion(int id, String name, String role, boolean enabled, boolean readOnly) {
 		this.id = id;
 		this.name = name;
+		this.role = role;
 		this.enabled = enabled;
 		this.readOnly = readOnly;
 	}
@@ -19,6 +21,10 @@ public class UserRegion {
 
 	public String getName() {
 		return name;
+	}
+	
+	public String getRole() {
+		return role;
 	}
 	
 	public boolean isEnabled() {
