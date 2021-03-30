c24962ba8fd4eb557c263ded77b6cbb813e9c843
joste <joste@server>
2021-03-30 11:52:20 +0200

    x

--- src/com/buldreinfo/jersey/jaxb/batch/FillProblems.java
+++ src/com/buldreinfo/jersey/jaxb/batch/FillProblems.java
@@ -121,7 +121,7 @@
 //		}
 //	}
 //	private final static int AUTH_USER_ID = 1;
-//	private final static int REGION_ID = 16;
+//	private final static int REGION_ID = 4;
 //	private final Setup setup;
 //
 //	public static void main(String[] args) {
@@ -132,7 +132,17 @@
 //		this.setup = new MetaHelper().getSetup(REGION_ID);
 //		List<Data> data = new ArrayList<>();
 //		// FA-date: yyyy-MM-dd
-//		data.add(new Data(1, "Sandnes and Gjesdal", "Kråkedal", "Krågedal-is", T.ICE, "50m - Short friendly option in the farmland. Walk off to climbers right.", 1, "WI3", null, null, 58.8411388888889, 5.93097222222222));
+//		data.add(new Data(1, "Skeisfjell", "Skeisfjell", "Svada", T.TRAD, "160m - Lite tiltalende, dårlig sikra og grufsete dieder på 3. tau. Fint riss i 4. taulengde", 4, "6-", "Leif Jensen,Eimhjellen", "1991-01-01", 0, 0));
+//		data.add(new Data(2, "Skeisfjell", "Skeisfjell", "Discovery", T.TRAD, "160m", 4, "5+", "Nils Engelstad,Thomas", "1980-06-01", 0, 0));
+//		data.add(new Data(3, "Skeisfjell", "Skeisfjell", "Discovery direkte avslutning", T.TRAD, "160m", 4, "5+", "Per Markestad,Harald Bjørgen", "1982-06-01", 0, 0));
+//		data.add(new Data(4, "Skeisfjell", "Skeisfjell", "Nogbad the bad", T.TRAD, "160m - Noe skuffende rute som krysser \"Nogging the Nog\"", 4, "6+", "Lee,Morley", "1987-08-01", 0, 0));
+//		data.add(new Data(5, "Skeisfjell", "Skeisfjell", "Nogging the nog", T.TRAD, "160m - Skeisfjells hardeste rute. Sentral linje opp midt på veggen. Starter med innlysende tynn sprekk, til venstre for et løst flak ca. 6m fra bakken.", 4, "7+", "Lee,Woodley", "1987-08-01", 0, 0));
+//		data.add(new Data(6, "Skeisfjell", "Skeisfjell", "Recovery", T.TRAD, "160m", 3, "5+", "Mike Blenkinsop", "1979-07-01", 0, 0));
+//		data.add(new Data(6, "Skeisfjell", "Skeisfjell", "Deflection", T.TRAD, "160m", 3, "5+", "Mike Blenkinsop,Holman", "1979-06-01", 0, 0));
+//		data.add(new Data(7, "Skeisfjell", "Skeisfjell", "Svenneprøven", T.TRAD, "Ny avslutning på Deflection (se detalj)", 3, "6-", "Leif Jensen,Bjordal", "1987-09-01", 0, 0));
+//		data.add(new Data(8, "Skeisfjell", "Skeisfjell", "Dragsug", T.TRAD, "90m - Starter under overheng. Fin bratt rute. På 2. tau er cruxet å komme inn i diederet. Fordel med flexi Friend no: 1.", 2, "7-", "Olav Båsen,Knutsen,Elder", "1989-07-01", 0, 0));
+//		data.add(new Data(9, "Skeisfjell", "Skeisfjell", "Trodde noen at friklatring var tøffest??", T.AIDTRAD, "120m (A4) - Start sammen med Rainstorm", 4, "5+", "Olav Båsen,Leif Jensen", "1990-10-01", 0, 0));
+//		data.add(new Data(10, "Skeisfjell", "Skeisfjell", "Rainstorm", T.TRAD, "130m", 3, "5+", "Mike Blenkinsop,John Fivelsdal", "1980-07-01", 0, 0));
 //
 //		try (DbConnection c = ConnectionPoolProvider.startTransaction()) {
 //			for (Data d : data) {
@@ -183,12 +193,12 @@
 //			}
 //		}
 //		Area a = new Area(REGION_ID, null, -1, false, false, false, d.getArea(), null, 0, 0, 0, 0, null, null, 0);
-//		a = c.getBuldreinfoRepo().setArea(AUTH_USER_ID, REGION_ID, a, null);
+//		a = c.getBuldreinfoRepo().setArea(new MetaHelper().getSetup(REGION_ID), AUTH_USER_ID, a, null);
 //		return a.getId();
 //	}
 //
 //	private int upsertSector(DbConnection c, int idArea, Data d) throws IOException, SQLException, NoSuchAlgorithmException, InterruptedException {
-//		Area a = Preconditions.checkNotNull(c.getBuldreinfoRepo().getArea(AUTH_USER_ID, REGION_ID, idArea));
+//		Area a = Preconditions.checkNotNull(c.getBuldreinfoRepo().getArea(new MetaHelper().getSetup(REGION_ID), AUTH_USER_ID, idArea));
 //		for (Area.Sector s : a.getSectors()) {
 //			if (s.getName().equals(d.getSector())) {
 //				return s.getId();
