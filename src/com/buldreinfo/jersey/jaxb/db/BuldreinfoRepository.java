f1376d0bc343ac600ae59a34b8b5da3c651a6488
Jostein Øygarden <jostein.oygarden@verico.com>
2021-04-20 13:45:47 +0200

    x

--- src/com/buldreinfo/jersey/jaxb/db/BuldreinfoRepository.java
+++ src/com/buldreinfo/jersey/jaxb/db/BuldreinfoRepository.java
@@ -757,16 +757,16 @@ public class BuldreinfoRepository {
 	public List<GradeDistribution> getGradeDistribution(int authUserId, Setup setup, int optionalAreaId, int optionalSectorId) throws SQLException {
 		List<GradeDistribution> res = new ArrayList<>();
 		String sqlStr = "WITH x AS ("
-				+ " SELECT x.sector, g.base_no grade_base_no, SUM(DISTINCT CASE WHEN x.prim=1 THEN 1 ELSE 0 END) prim, SUM(CASE WHEN x.prim=0 THEN 1 ELSE 0 END) sec"
-				+ " FROM (SELECT s.name sector, ROUND((IFNULL(AVG(NULLIF(t.grade,0)), p.grade) + p.grade)/2) grade_id, CASE WHEN p.type_id IN (1,2) THEN 1 ELSE 0 END prim, p.id id_problem"
+				+ " SELECT x.sorting, x.sector, g.base_no grade_base_no, COUNT(DISTINCT CASE WHEN x.prim=1 THEN x.id_problem END) prim, COUNT(DISTINCT CASE WHEN x.prim=0 THEN x.id_problem END) sec"
+				+ " FROM (SELECT s.name sector, s.sorting, ROUND((IFNULL(AVG(NULLIF(t.grade,0)), p.grade) + p.grade)/2) grade_id, CASE WHEN p.type_id IN (1,2) THEN 1 ELSE 0 END prim, p.id id_problem"
 				+ " FROM ((((area a INNER JOIN sector s ON a.id=s.area_id) INNER JOIN problem p ON s.id=p.sector_id) INNER JOIN type ty ON p.type_id=ty.id) LEFT JOIN user_region ur ON a.region_id=ur.region_id AND ur.user_id=?) LEFT JOIN tick t ON p.id=t.problem_id"
 				+ (optionalAreaId!=0? " WHERE a.id=?" : " WHERE p.sector_id=?")
 				+ "   AND is_readable(ur.admin_read, ur.superadmin_read, p.locked_admin, p.locked_superadmin)=1"
 				+ " GROUP BY s.name, p.type_id, p.id) x, grade g"
 				+ " WHERE x.grade_id=g.grade_id AND g.t=?"
-				+ " GROUP BY sector, g.base_no"
+				+ " GROUP BY x.sorting, x.sector, g.base_no"
 				+ " )"
-				+ " SELECT g.base_no grade, SUM(x.prim) prim, SUM(x.sec) sec, group_concat(concat(x.sector,': ',(x.prim+x.sec)) order by x.sector separator ', ') tooltip"
+				+ " SELECT g.base_no grade, SUM(x.prim) prim, SUM(x.sec) sec, group_concat(concat(x.sector,': ',(x.prim+x.sec)) order by x.sorting, x.sector separator ', ') tooltip"
 				+ " FROM (SELECT g.base_no, MIN(g.grade_id) sort FROM grade g WHERE g.t=? GROUP BY g.base_no) g LEFT JOIN x ON g.base_no=x.grade_base_no"
 				+ " GROUP BY g.base_no, g.sort"
 				+ " ORDER BY g.sort";
