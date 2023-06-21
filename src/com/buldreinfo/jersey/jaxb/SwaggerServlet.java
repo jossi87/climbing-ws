4ad8407eede0095ed4ca748381f19978948c245e
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2023-06-21 08:59:53 +0200

    x

--- src/com/buldreinfo/jersey/jaxb/SwaggerServlet.java
+++ src/com/buldreinfo/jersey/jaxb/SwaggerServlet.java
@@ -9,17 +9,18 @@ import io.swagger.jaxrs.config.BeanConfig;
 /**
  * Creates https://brattelinjer.no/com.buldreinfo.jersey.jaxb/swagger.json
  */
-@SuppressWarnings("serial")
 public class SwaggerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
 	@Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.0");
+        beanConfig.setVersion("2.0.0");
         beanConfig.setSchemes(new String[]{"https"});
         beanConfig.setHost("brattelinjer.no");
         beanConfig.setBasePath("/com.buldreinfo.jersey.jaxb");
-        beanConfig.setResourcePackage("com.buldreinfo.jersey.jaxb");
+        beanConfig.setResourcePackage("io.swagger.resources");
         beanConfig.setScan(true);
     }
 }
