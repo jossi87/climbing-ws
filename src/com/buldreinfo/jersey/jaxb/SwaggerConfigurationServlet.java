032a3bf6ed173e73ce7114686aba1f5a3c165ea0
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2023-06-21 09:55:53 +0200

    x

--- src/com/buldreinfo/jersey/jaxb/SwaggerConfigurationServlet.java
+++ src/com/buldreinfo/jersey/jaxb/SwaggerConfigurationServlet.java
@@ -1,12 +1,8 @@
 package com.buldreinfo.jersey.jaxb;
 
-import java.io.IOException;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import io.swagger.jaxrs.config.BeanConfig;
 
@@ -15,23 +11,17 @@ import io.swagger.jaxrs.config.BeanConfig;
  */
 public class SwaggerConfigurationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private BeanConfig beanConfig;
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		beanConfig = new BeanConfig();
+		BeanConfig beanConfig = new BeanConfig();
 		beanConfig.setVersion("2.0.0");
 		beanConfig.setSchemes(new String[]{"https"});
 		beanConfig.setHost("brattelinjer.no");
 		beanConfig.setBasePath("/com.buldreinfo.jersey.jaxb");
 		beanConfig.setResourcePackage("com.buldreinfo.jersey.jaxb");
 		beanConfig.setScan(true);
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		beanConfig.setHost("test123");
-		super.doGet(req, resp);
+		beanConfig.setContact("https://github.com/jossi87");
 	}
 }
