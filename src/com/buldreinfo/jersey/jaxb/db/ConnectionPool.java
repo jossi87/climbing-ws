2226fb5b3c3e2cee34770cb4f5aaf24a50483cb7
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2023-06-30 12:32:01 +0200

    moved config to properties file

--- src/com/buldreinfo/jersey/jaxb/db/ConnectionPool.java
+++ src/com/buldreinfo/jersey/jaxb/db/ConnectionPool.java
@@ -4,23 +4,27 @@ import java.sql.SQLException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
+import com.buldreinfo.jersey.jaxb.config.BuldreinfoConfig;
 
 /**
  * @author <a href="mailto:jostein.oygarden@gmail.com">Jostein Oeygarden</a>
  */
 public class ConnectionPool {
 	private final BasicDataSource bds;
-	
+
 	public ConnectionPool() {
-		final String buldreinfoDb = Preconditions.checkNotNull(Strings.emptyToNull(System.getenv("buldreinfo_db")), "Could not find environment variable \"buldreinfo_db\". Expected a value with the following format: \"jdbc:mysql://HOST/DATABASE?user=USER&password=PASSWORD&serverTimezone=UTC\"");
-        this.bds = new BasicDataSource();
+		BuldreinfoConfig config = BuldreinfoConfig.getConfig();
+		String hostname = config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_HOSTNAME);
+		String database = config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_DATABASE);
+		String username = config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_USERNAME);
+		String password = config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_PASSWORD);
+		String url = String.format("jdbc:mysql://%s/%s?user=%s&password=%s&serverTimezone=UTC", hostname, database, username, password);
+		this.bds = new BasicDataSource();
 		this.bds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		this.bds.setUrl(buldreinfoDb);
+		this.bds.setUrl(url);
 		this.bds.setMaxTotal(64);
 	}
-	
+
 	protected BasicDataSource getBasicDataSource() throws SQLException {
 		return bds;
 	}
