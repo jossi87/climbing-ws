f643d943d607d524c77663a2c66532d65300566f
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2023-08-20 09:58:33 +0200

    temp redirect

--- src/com/buldreinfo/jersey/jaxb/V2.java
+++ src/com/buldreinfo/jersey/jaxb/V2.java
@@ -180,7 +180,7 @@ public class V2 {
 					// Area not found, see if it's visible on a different domain
 					Redirect res = c.getBuldreinfoRepo().getCanonicalUrl(id, 0, 0);
 					if (!Strings.isNullOrEmpty(res.getRedirectUrl())) {
-						response = Response.seeOther(new URI(res.getRedirectUrl())).build();
+						response = Response.temporaryRedirect(new URI(res.getRedirectUrl())).build();
 					}
 					else {
 						logger.fatal(e.getMessage(), e);
