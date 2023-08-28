c473cc6d7e9c594b225ce24e3dc782fc7cbc0c56
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2023-08-28 09:18:00 +0200

    x

--- src/com/buldreinfo/jersey/jaxb/model/ActivityMedia.java
+++ src/com/buldreinfo/jersey/jaxb/model/ActivityMedia.java
@@ -3,13 +3,13 @@ package com.buldreinfo.jersey.jaxb.model;
 public class ActivityMedia {
 	private final int id;
 	private final int crc32;
-	private final boolean isMovie;
+	private final boolean movie;
 	private final String embedUrl;
 	
-	public ActivityMedia(int id, int crc32, boolean isMovie, String embedUrl) {
+	public ActivityMedia(int id, int crc32, boolean movie, String embedUrl) {
 		this.id = id;
 		this.crc32 = crc32;
-		this.isMovie = isMovie;
+		this.movie = movie;
 		this.embedUrl = embedUrl;
 	}
 	
@@ -26,11 +26,11 @@ public class ActivityMedia {
 	}
 	
 	public boolean isMovie() {
-		return isMovie;
+		return movie;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Media [id=" + id + ", isMovie=" + isMovie + ", embedUrl=" + embedUrl + "]";
+		return "Media [id=" + id + ", movie=" + movie + ", embedUrl=" + embedUrl + "]";
 	}
 }
