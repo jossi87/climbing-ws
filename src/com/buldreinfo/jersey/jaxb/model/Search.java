f71e43e3b2d7ccdddfad96f737d30a3b10a67acc
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2024-02-27 12:25:30 +0100

    search properties in lower case

--- src/com/buldreinfo/jersey/jaxb/model/Search.java
+++ src/com/buldreinfo/jersey/jaxb/model/Search.java
@@ -1,3 +1,3 @@
 package com.buldreinfo.jersey.jaxb.model;
 
-public record Search(String title, String description, String url, String externalurl, String mediaurl, int mediaid, int crc32, boolean lockedAdmin, boolean lockedSuperadmin) {}
+public record Search(String title, String description, String url, String externalurl, String mediaurl, int mediaid, int crc32, boolean lockedadmin, boolean lockedsuperadmin) {}
