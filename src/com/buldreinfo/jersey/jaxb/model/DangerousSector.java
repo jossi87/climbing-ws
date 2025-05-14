78b16687ecbe60795114659d69936c194b98b0ff
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2025-05-14 08:28:35 +0200

    sun on wall

--- src/com/buldreinfo/jersey/jaxb/model/DangerousSector.java
+++ src/com/buldreinfo/jersey/jaxb/model/DangerousSector.java
@@ -2,4 +2,4 @@ package com.buldreinfo.jersey.jaxb.model;
 
 import java.util.List;
 
-public record DangerousSector(int id, String url, String name, CompassDirection wallDirectionCalculated, CompassDirection wallDirectionManual, boolean lockedAdmin, boolean lockedSuperadmin, List<DangerousProblem> problems) {}
+public record DangerousSector(int id, String url, String name, CompassDirection wallDirectionCalculated, CompassDirection wallDirectionManual, boolean lockedAdmin, boolean lockedSuperadmin, int sunFromHour, int sunToHour, List<DangerousProblem> problems) {}
