3dbedbb45e28069f1f032f3e3bf83446c344d4e5
Jostein Øygarden <jostein.oygarden@esmartsystems.com>
2025-12-18 08:06:34 +0100

    changed to HikariCP

--- src/com/buldreinfo/jersey/jaxb/Server.java
+++ src/com/buldreinfo/jersey/jaxb/Server.java
@@ -1,11 +1,11 @@
 package com.buldreinfo.jersey.jaxb;
 
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,25 +19,33 @@ import com.buldreinfo.jersey.jaxb.function.FunctionDbUser;
 import com.buldreinfo.jersey.jaxb.helpers.AuthHelper;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Stopwatch;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.core.Response;
 
 public class Server {
-	private static Server server;
+	private static volatile Server server;
 	private static Logger logger = LogManager.getLogger();
 
-	public static List<Setup> getSetups() {
-		return getServer().setups;
+	public static Collection<Setup> getSetups() {
+		return getServer().setupMap.values();
 	}
 
 	public static void runSql(Consumer<Connection> action) {
 		Server server = getServer();
-		try (Connection c = server.bds.getConnection()) {
-			c.setAutoCommit(false);
-			action.run(server.dao, c);
-			if (!c.getAutoCommit()) {
-				c.commit();
+		try (Connection c = server.ds.getConnection()) {
+			try {
+				action.run(server.dao, c);
+				if (!c.getAutoCommit()) {
+					c.commit();
+				}
+			} catch (Exception e) {
+				if (!c.getAutoCommit()) {
+					c.rollback();
+				}
+				throw e;
 			}
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
@@ -65,11 +73,15 @@ public class Server {
 	protected static Response buildResponseWithSql(HttpServletRequest request, FunctionDb<Connection, Response> function) {
 		Server server = getServer();
 		Setup setup = server.getSetup(request);
-		try (Connection c = server.bds.getConnection()) {
-			c.setAutoCommit(false);
-			Response res = function.get(server.dao, c, setup);
-			c.commit();
-			return res;
+		try (Connection c = server.ds.getConnection()) {
+			try {
+				Response res = function.get(server.dao, c, setup);
+				c.commit();
+				return res;
+			} catch (Exception e) {
+				c.rollback();
+				throw e;
+			}
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			return Response.serverError().build();
@@ -79,51 +91,66 @@ public class Server {
 	protected static Response buildResponseWithSqlAndAuth(HttpServletRequest request, FunctionDbUser<Connection, Response> function) {
 		Server server = getServer();
 		Setup setup = server.getSetup(request);
-		try (Connection c = server.bds.getConnection()) {
-			c.setAutoCommit(false);
-			Optional<Integer> authUserId = server.auth.getAuthUserId(server.dao, c, request, setup);
-			Response res = function.get(server.dao, c, setup, authUserId);
-			c.commit();
-			return res;
+		try (Connection c = server.ds.getConnection()) {
+			try {
+				Optional<Integer> authUserId = server.auth.getAuthUserId(server.dao, c, request, setup);
+				Response res = function.get(server.dao, c, setup, authUserId);
+				c.commit();
+				return res;
+			} catch (Exception e) {
+				c.rollback();
+				throw e;
+			}
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			return Response.serverError().build();
 		}
 	}
 
-	private final BasicDataSource bds;
+	private final HikariDataSource ds;
 	private final Dao dao = new Dao();
 	private final AuthHelper auth = new AuthHelper();
-	private final List<Setup> setups = new ArrayList<>();
+	private final Map<String, Setup> setupMap = new ConcurrentHashMap<>();
 
 	private Server() {
 		Stopwatch stopwatch = Stopwatch.createStarted();
 		BuldreinfoConfig config = BuldreinfoConfig.getConfig();
-		String hostname = config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_HOSTNAME);
-		String database = config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_DATABASE);
-		String username = config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_USERNAME);
-		String password = config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_PASSWORD);
-		String url = String.format("jdbc:mysql://%s/%s?user=%s&password=%s&serverTimezone=UTC", hostname, database, username, password);
-		this.bds = new BasicDataSource();
-		this.bds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		this.bds.setUrl(url);
-		this.bds.setMaxTotal(64);
-		try (Connection c = bds.getConnection()) {
-			setups.addAll(dao.getSetups(c));
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setJdbcUrl(String.format("jdbc:mysql://%s/%s", 
+				config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_HOSTNAME),
+				config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_DATABASE)));
+		hikariConfig.setUsername(config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_USERNAME));
+		hikariConfig.setPassword(config.getProperty(BuldreinfoConfig.PROPERTY_KEY_DB_PASSWORD));
+		hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		hikariConfig.setAutoCommit(false);
+		hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+		hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+		hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		hikariConfig.addDataSourceProperty("useServerPrepStmts", "true");
+		hikariConfig.setMaximumPoolSize(10); 
+		hikariConfig.setMinimumIdle(5);
+		hikariConfig.setConnectionTimeout(10000);
+		hikariConfig.setLeakDetectionThreshold(4000);
+		this.ds = new HikariDataSource(hikariConfig);
+		try (Connection c = ds.getConnection()) {
+			dao.getSetups(c).forEach(s -> {
+				setupMap.put(s.domain().toLowerCase(), s);
+			});
 		} catch (Exception e) {
 			logger.error(e.getMessage(), e);
 			throw new RuntimeException(e.getMessage(), e);
 		}
-		logger.debug("Server initialized in {}", stopwatch);
+		logger.info("Server initialized in {}", stopwatch);
 	}
 
 	private Setup getSetup(HttpServletRequest request) {
 		Preconditions.checkNotNull(request);
 		Preconditions.checkNotNull(request.getServerName(), "Invalid request=" + request);
 		final String serverName = request.getServerName().toLowerCase().replace("www.", "");
-		return setups.stream()
-				.filter(x -> serverName.equalsIgnoreCase(x.domain()))
-				.findAny()
-				.orElseThrow(() -> new RuntimeException("Invalid serverName=" + serverName));
+		Setup setup = setupMap.get(serverName);
+		if (setup == null) {
+			throw new RuntimeException("Invalid serverName=" + serverName);
+		}
+		return setup;
 	}
 }
