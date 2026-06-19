01893bf8e85612cd0267bb4b22ec0b4b10015daa
Jostein Øygarden <jostein.oygarden@gmail.com>
2026-06-19 14:58:12 +0200

    api

--- src/main/java/com/buldreinfo/jersey/jaxb/infrastructure/ApiApplication.java
+++ src/main/java/com/buldreinfo/jersey/jaxb/infrastructure/ApiApplication.java
@@ -4,17 +4,13 @@ import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import com.buldreinfo.jersey.jaxb.filters.HitTrackingFilter;
-import com.buldreinfo.jersey.jaxb.resources.BaseResource;
 
-import jakarta.ws.rs.ApplicationPath;
-
-@ApplicationPath("/")
 public class ApiApplication extends ResourceConfig {
     public ApiApplication() {
         super(MultiPartFeature.class);
         
         packages(
-            BaseResource.class.getPackageName(),
+            "com.buldreinfo.jersey.jaxb.resources",
             "io.swagger.v3.jaxrs2.integration.resources"
         );
         
