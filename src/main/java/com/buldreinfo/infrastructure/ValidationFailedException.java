2d9c2d8f54ceb81c43035284eb1eb8067c9de74e
Jostein Øygarden <jostein.oygarden@gmail.com>
2026-06-23 10:20:39 +0200

    added missing file

---
+++ src/main/java/com/buldreinfo/infrastructure/ValidationFailedException.java
@@ -0,0 +1,9 @@
+package com.buldreinfo.infrastructure;
+
+public class ValidationFailedException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ValidationFailedException(String message) {
+        super(message);
+    }
+}
