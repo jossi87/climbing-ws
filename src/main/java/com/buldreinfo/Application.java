a3e28a9bfe72ea48c671c3117d5ae68a75ccf3e9
Jostein Øygarden <jostein.oygarden@gmail.com>
2026-06-25 20:23:02 +0200

    removed old tomcat-stuff now that we are using fat jar

--- src/main/java/com/buldreinfo/Application.java
+++ src/main/java/com/buldreinfo/Application.java
@@ -2,20 +2,13 @@ package com.buldreinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ConfigurationPropertiesScan("com.buldreinfo.config")
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
