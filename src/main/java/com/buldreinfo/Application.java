d04d970c5095164c6fb251c6a4348a573d5564d0
Jostein Øygarden <jostein.oygarden@gmail.com>
2026-07-03 09:17:21 +0200

    logging

--- src/main/java/com/buldreinfo/Application.java
+++ src/main/java/com/buldreinfo/Application.java
@@ -3,11 +3,11 @@ package com.buldreinfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@ConfigurationPropertiesScan("com.buldreinfo.config")
-@SpringBootApplication
+@ConfigurationPropertiesScan
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
