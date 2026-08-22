daca1a9d6f12f4484fa1af4240d4b6de1db5cf49
Jostein Øygarden <jostein.oygarden@gmail.com>
2026-08-22 15:24:32 +0200

    include numPitches

--- src/main/java/com/buldreinfo/model/ProfileTodo.java
+++ src/main/java/com/buldreinfo/model/ProfileTodo.java
@@ -5,5 +5,5 @@ import java.util.List;
 public record ProfileTodo(List<ProfileTodoArea> areas) {
 	public record ProfileTodoArea(int id, String name, boolean lockedAdmin, boolean lockedSuperadmin, List<ProfileTodoSector> sectors) {}
 	public record ProfileTodoSector(int id, String name, boolean lockedAdmin, boolean lockedSuperadmin, List<ProfileTodoProblem> problems) {}
-	public record ProfileTodoProblem(int todoId, int id, boolean lockedAdmin, boolean lockedSuperadmin, int nr, String name, String grade, String subType, Coordinates coordinates, List<User> partners) {}
+	public record ProfileTodoProblem(int todoId, int id, boolean lockedAdmin, boolean lockedSuperadmin, int nr, String name, String grade, String subType, int numPitches, Coordinates coordinates, List<User> partners) {}
 }
